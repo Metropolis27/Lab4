a8f9edad68df6cf95cc87d49092fbc51f646c3d6
Metropolis27 <104534803+Metropolis27@users.noreply.github.com>
2023-11-14 23:27:47 +0000

    Write Your First Bash Script Complete

---
+++ status.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Script that returns the current server status
+
+server_name=$(hostname)
+
+echo $server_name
+
+function function_name() {
+    your_commands
+}
+
+#Define the function
+function memory_check() {
+    echo ""
+	echo "The current memory usage on ${server_name} is: "
+	free -h
+	echo ""
+}
+
+# Call the function
+memory_check
